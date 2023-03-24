f76913c104f1508dcb97d1a9776e9ac80ae779cb
Zwirled <bramwellamelia@gmail.com>
2023-03-24 21:14:10 +0000

    Fixed swiper to include functional arrow navigation

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,23 +1,38 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import './style.css';
+import { Navigation } from "swiper";
 
 function Hero() {
     const images = ['slider-image-1.webp', 'slider-image-2.webp', 'slider-image-3.webp', 'slider-image-4.webp', 'slider-image-5.jpeg'];
 
     return (
-        <Swiper
-            spaceBetween={0}
-            slidesPerView={1}
-            navigation={true}
-            pagination={true}
-        >
-            {images.map((image, index) => (
-                <SwiperSlide key={index}>
-                    <img src={process.env.PUBLIC_URL + '/slider/' + image} alt={'Slide ' + (index + 1)} />
-                </SwiperSlide>
-            ))}
-        </Swiper>
+        <div class="heroSlider">
+            <div className="swiper-button-prev swiper-arrow">
+                <img src={process.env.PUBLIC_URL + '/slider/arrow-prev.svg'} alt='prev' />
+            </div>
+            <div className="swiper-button-next swiper-arrow">
+                <img src={process.env.PUBLIC_URL + '/slider/arrow-next.svg'} alt='next' />
+            </div>
+            <Swiper
+                spaceBetween={0}
+                slidesPerView={1}
+                onSlideChange={() => console.log("slide change")}
+                onSwiper={(swiper) => console.log(swiper)}
+                navigation={{
+                    prevEl: ".swiper-button-prev",
+                    nextEl: ".swiper-button-next",
+                }}
+                modules={[Navigation]}
+                pagination={true}
+            >
+                {images.map((image, index) => (
+                    <SwiperSlide key={index}>
+                        <img src={process.env.PUBLIC_URL + '/slider/' + image} alt={'Slide ' + (index + 1)} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
     );
 }
 
