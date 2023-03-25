37539fc073d11437194338234bb70ba610ccf028
Zwirled <bramwellamelia@gmail.com>
2023-03-25 13:28:20 +0000

    Removed unused imports from Recipes page

--- src/pages/Recipes/index.js
+++ src/pages/Recipes/index.js
@@ -1,14 +1,5 @@
 import React from "react";
 import "./style.css";
-import { Grid } from "@mui/material";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { Button, CardActionArea, CardActions } from "@mui/material";
-import ButtonUnstyled from "@mui/base/ButtonUnstyled";
-import foodPlaceholder from "../../images/foodPlaceHolder.jpeg";
-import Container from '@mui/material/Container';
 import RecipeFeed from '../../components/RecipeFeed';
 
 function Recipes() {
